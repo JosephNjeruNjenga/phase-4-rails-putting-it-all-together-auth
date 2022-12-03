8e8a7f148fd1f3372de9e9791fe1d1471bec332f
+ <joseph.njenga@student.moringaschool.com>
2022-12-03 21:15:31 +0300

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  def render_unprocessable_entity_response(e)
+    render json: {errors: [e.record.errors]}, status: :unprocessable_entity
+  end
 end
